1f64dcb54c50c11f24c5dbfa26f29d9b2b4459fc
joksulainen <minna1970.mn@gmail.com>
2021-10-12 21:49:12 +0300

    Progress

--- sub_tracker.py
+++ sub_tracker.py
@@ -4,13 +4,103 @@ from time import sleep
 from typing import List
 
 class SubTracker:
-    thread: threading.Thread = None
-    name: str = ""
-    account_id: int = None
-    update_frequency: float = 3
+    thread: threading.Thread
+    token: str
+    account_id: int
+    pfp: str
+    update_frequency: float
+    webhooks: List[str]
+    old_subs: int
 
-    def __init__(self, name: str = "", account_id: int, update_frequency: float = 3):
-        self.thread = threading.Thread(target=self.sub_tracker)
+    def __init__(self, token: str, account_id: int, update_frequency: float = 3):
+        self.account_id = account_id
+        self.token = token
+        self.update_frequency = update_frequency
+        r = requests.get(f"https://accounts.rec.net/account/{self.account_id}")
+        self.thread = threading.Thread(target=self.sub_tracker, name=r['username'])
+        self.pfp = "https://img.rec.net/" + r["profileImage"]
+        self.old_subs = self.fetch_subscribers()['subs']
+
+
+    def fetch_subscribers(self):
+        """Fetch subscriber count from the rec.net servers."""
+        # Send GET request to request sub count.
+        r = requests.get(
+            f"https://clubs.rec.net/subscription/subscriberCount/{self.account_id}",
+            headers={"Authorization": self.token}
+        )
+        # Return a failed fetch attempt.
+        if not r.ok:
+            return {"success": False}
+
+        subs = int(r.text)
+        print(f"[{self.thread.name}] Subscribers:", subs)
+
+        # Return success with sub count.
+        return {"success": True, "subs": subs}
+
 
     def sub_tracker(self):
-        pass
+        """Sub tracker loop."""
+        while True:
+            # Fetch sub count.
+            sub_fetch = self.fetch_subscribers()
+            # Login if the fetch attempt was unsuccessful.
+            if not sub_fetch['success']:
+                login = login_to_recnet(os.environ["RR_USERNAME"], os.environ["RR_PASSWORD"])
+                # Exit process if login failed. Otherwise set new token and continue.
+                if not login['success']:
+                    print(f"[{self.thread.name}] Invalid RR login details")
+                    break
+                self.token = login['bearer_token']
+                continue
+
+            subs = sub_fetch['subs']
+
+            # Post embeds of sub increase or decrease if applicable.
+            if subs > self.old_subs:
+                print(f"[{self.thread.name}] Gained subs!", subs-self.old_subs)
+                payload = {
+                    "embeds": [
+                        {
+                            "title": "Gained subscribers!",
+                            "description": f"{self.old_subs:,} (+{subs-self.old_subs})\n**Subscribers:** `{subs:,}`",
+                            "color": 0xE67E22,
+                            "thumbnail": {"url": self.pfp},
+                            "footer": {"text": f"Account: {self.thread.name}"}
+                        }
+                    ]
+                }
+                for url in self.webhooks:
+                    r = requests.post(url, json=payload, timeout=3)
+                    if not r.ok:
+                        print(f"[{self.thread.name}] POST request failed\n{url}")
+                self.old_subs = subs
+            elif subs < self.old_subs:
+                print(f"[{self.thread.name}] Lost subs!", self.old_subs-subs)
+                payload = {
+                    "embeds": [
+                        {
+                            "title": "Lost subscribers!",
+                            "description": f"{self.old_subs:,} (-{self.old_subs-subs})\n**Subscribers:** `{subs:,}`",
+                            "color": 0xE67E22,
+                            "thumbnail": {"url": self.pfp},
+                            "footer": {"text": f"Account: {self.thread.name}"}
+                        }
+                    ]
+                }
+                for url in self.webhooks:
+                    r = requests.post(url, json=payload, timeout=3)
+                    if not r.ok:
+                        print(f"[{self.thread.name}] POST request failed\n{url}")
+                self.old_subs = subs
+            else:
+                print(f"[{self.thread.name}] No sub change.")
+            
+            # Wait the given time before checking again.
+            sleep(self.update_frequency)
+
+        # Terminate thread if broken out of loop
+        print(f"[{self.thread.name}] Terminating thread...")
+        self.thread.join()
+        print(f"[{self.thread.name}] Thread terminated")
