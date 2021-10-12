ccf6c2d2af1c3830e4515e2e18b2a81912bb7892
joksulainen <minna1970.mn@gmail.com>
2021-10-12 18:56:15 +0300

    Class base progress

--- sub_tracker.py
+++ sub_tracker.py
@@ -4,9 +4,13 @@ from time import sleep
 from typing import List
 
 class SubTracker:
+    thread: threading.Thread = None
+    name: str = ""
+    account_id: int = None
+    update_frequency: float = 3
 
-    def __init__(self, name, account_id):
-        self.thread = threading.Thread(target=self.sub_tracker, daemon=True)
+    def __init__(self, name: str = "", account_id: int, update_frequency: float = 3):
+        self.thread = threading.Thread(target=self.sub_tracker)
 
     def sub_tracker(self):
         pass
